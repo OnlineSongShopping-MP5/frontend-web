21579bc9b6cb36db01d3c345670ee5d75acb56d4
jzx0902 <jzx0902@hotmail.com>
2016-04-19 09:24:44 -0400

    Added files via upload

--- 9.6_recommend_songs_age_group.php
+++ 9.6_recommend_songs_age_group.php
@@ -13,12 +13,15 @@
 
 $conn = oci_connect('zjia', '1A2b3c4d!!', 'oracle.cise.ufl.edu:1521/orcl');
 session_start();
+if(isset($_POST['Submit'])){
 
-$recommend = 	"select song.title
+        $startage = $_POST['startage'];
+        $endage = $_POST['endage'];
+        $recommend = 	"select song.title
 		from song,
 		(select orders.song_id as oid
 		from customer, orders
-		where customer.age >= 20 and customer.age <= 30 and customer.username = orders.customer
+		where customer.age >= :startage and customer.age <= :endage and customer.username = orders.customer
 		group by orders.song_id
 		order by count(*) desc)
 		where song.id = oid
@@ -26,44 +29,74 @@ $recommend = 	"select song.title
 
 $s_select_all = oci_parse($conn, $recommend);
 oci_bind_by_name($s_select_all, ':username', $_SESSION['userName']);
-
-
+oci_bind_by_name($s_select_all, ':startage', $startage);
+oci_bind_by_name($s_select_all, ':endage', $endage);
 //oci_bind_by_name($sss, ':searchprice111', $_SESSION['searchprice']);
         
-oci_execute($s_select_all); 
+oci_execute($s_select_all);  
+}
+
+
 
 ?>
 
 
 <?php
+//
+//echo "<table width='300' border='2' align = 'center' >
+//
+//	<tr>
+//
+//	<td width = '100'> No. </td>
+//	<td width = '200'> Song Name </td>
+//
+//	</tr>";
+//
+//$i = 1;
+//
+//	while ($row = oci_fetch_array($s_select_all, OCI_BOTH)) {
+//
+//	    echo "<tr>";
+//
+//	    echo "<td>" . $i	   	. "</td>";
+//	    echo "<td>" . $row['TITLE']  . "</td>";
+//
+//	    echo "</tr>";
+//            
+//         	$i++;
+//
+//
+//	}
+//
+//echo "</table>";
+//    
+//?>
+    <?php
 
-echo "<table width='300' border='2' align = 'center' >
-
-	<tr>
-
-	<td width = '100'> No. </td>
-	<td width = '200'> Song Name </td>
-
-	</tr>";
-
+echo "
+<form action = '' method = 'post'>
+<table width='300' border='2' align = 'center' >
+ <td><input type='submit' name='Submit' id='Submit' value='Submit'></td>";
+echo " <tr>
+      <td>Start age:</td>
+      <td><input name = 'startage' type='text'></td>
+    </tr>
+    <tr>
+      <td>End age:</td>
+      <td><input name = 'endage' type='text'></td>
+    </tr>";
 $i = 1;
+while ($row = oci_fetch_array($s_select_all, OCI_BOTH)) {
+    echo "<tr>";
+    echo "<td>" . $i	   	. "</td>";
+    echo " <td> ".$row['TITLE']."</td>";
+    echo "</tr>";
+    $i++;
+}
+echo " 
+</table>
+</form>";
 
-	while ($row = oci_fetch_array($s_select_all, OCI_BOTH)) {
-
-	    echo "<tr>";
-
-	    echo "<td>" . $i	   	. "</td>";
-	    echo "<td>" . $row['TITLE']  . "</td>";
-
-	    echo "</tr>";
-            
-         	$i++;
-
-
-	}
-
-echo "</table>";
-    
 ?>
 
 </body>
